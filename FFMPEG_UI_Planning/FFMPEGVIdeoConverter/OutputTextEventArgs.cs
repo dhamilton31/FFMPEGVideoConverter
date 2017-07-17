7cc7a2bc87dac8769f53c4424399f66cdf7c05b3
CORP\dh185148 <dh185148@ncr.com>
2017-07-17 16:40:58 -0400

    OutputTextEventArgs cs file added.

---
+++ FFMPEG_UI_Planning/FFMPEGVIdeoConverter/OutputTextEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFMPEGVIdeoConverter
+{
+    public class OutputTextEventArgs : EventArgs
+    {
+        private List<string> outputText;
+
+        public OutputTextEventArgs()
+        {
+            outputText = new List<string>();
+        }
+
+        public void AddTextToOutput(string text)
+        {
+            outputText.Add(text);
+        }
+
+        public List<string> ReadOutputText()
+        {
+            return outputText;
+        }
+    }
+}
